4f4d8ee16c489b23b54dc6032b1872d98f730f84
gZEVI-1 <sergorbachev27@gmail.com>
2024-11-28 19:13:57 +0400

    Update Time.h

--- model/Time.h
+++ model/Time.h
@@ -2,15 +2,28 @@
 #include <iostream>
 #include <sstream>
 #include <string>
-
-struct Time
-{
-	int hour = -1, min = -1;
-};
+#include <iostream>
 
 using std::string;
+using std::cin;
+using std::cout;
 
-bool is_only_number(string number) //в строке только числа?
+int time_in_min(string time);
+bool Check_time(string time);
+
+int input_time() {
+	string input_str;
+	cin >> input_str;
+
+	if (Check_time(input_str)) {
+		return time_in_min(input_str);
+	}
+	else {
+		return -1;
+	}
+}
+
+bool is_only_number(string number)
 {
 	for (int i = 0; i < number.length(); i++) {
 		int char_in_ascii = number[i];
@@ -26,15 +39,17 @@ bool Check_time(string time)
 	int index_sep = time.find(':');
 	if (index_sep <= 0) { return false; }
 
-	string hours_str = time.substr(0, index_sep); //взятие символов, отвечающих за часы
-	string minutes_str = time.substr(index_sep + 1); //взятие символов, отвечающих за минуты
+	string hours_str = time.substr(0, index_sep);
+	string minutes_str = time.substr(index_sep + 1);
+
 
 	if (!(is_only_number(hours_str)) || !(is_only_number(minutes_str)) || hours_str.size() > 2 || minutes_str.size() > 2) {
 		return false;
 	}
 
-	int hours_int = stoi(hours_str); //перевод строки, содержащую символы часов, в int
-	int minutes_int = stoi(minutes_str); //перевод строки, содержащую символы минут, в int
+	int hours_int = stoi(hours_str);
+	int minutes_int = stoi(minutes_str);
+
 
 	if (hours_int > 23 || minutes_int > 59 || hours_int < 0 || minutes_int < 0) {
 		return false;
@@ -42,22 +57,7 @@ bool Check_time(string time)
 	return true;
 }
 
-Time Normal_view_time_struct(string time) {// время в стракте (в интах) с проверкой 
-	if (Check_time(time)) {
-		int index_sep = time.find(':');
-
-		string hours_str = time.substr(0, index_sep);
-		string minutes_str = time.substr(index_sep + 1);
-
-		Time return_time;
-		return_time.hour = stoi(hours_str);
-		return_time.min = stoi(minutes_str);
-
-		return return_time;
-	}
-}
-
-int Normal_view_time_min(string time) {//время в минутах с проверкой
+int time_in_min(string time) {
 	if (Check_time(time)) {
 		int index_sep = time.find(':');
 
